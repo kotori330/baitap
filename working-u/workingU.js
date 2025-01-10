1dba659e103ba5c9191c2ddcaedc9dddce0f547c
kotori330 <kemnguyen278@gmail.com>
2025-01-10 17:22:51 +0700

    Refactor WorkingU

--- working-u/workingU.js
+++ working-u/workingU.js
@@ -18,223 +18,261 @@ loadComponents("header", `${path}/components/header.html`);
 loadComponents("intro", `${path}/components/intro.html`);
 loadComponents("mini-navbar", `${path}/myPage/miniNavbar.html`);
 
+document.addEventListener("DOMContentLoaded", () => {
+  const dynamicContentSection = document.getElementById("dynamic-content");
+  const basicInfo = document.getElementById("basic-info");
+  const wageInfo = document.getElementById("wage-info");
+  const familyInfo = document.getElementById("family-info");
+  const parentalLeaveInfo = document.getElementById("parental-leave-info");
+  const accountSettings = document.getElementById("account-settings");
 
-const dynamicContentSection = document.getElementById("dynamic-content");
-const basicInfo = document.getElementById("basic-info");
-const wageInfo = document.getElementById("wage-info");
-const familyInfo = document.getElementById("family-info");
-const parentalLeaveInfo = document.getElementById("parental-leave-info");
-const accountSettings = document.getElementById("account-settings");
+  const birthReportModal = document.getElementById("birth-report-modal");
+  const birthReportModalContent = document.getElementById("modal-1");
+  const birthReportBtn = document.getElementById("give-birth-btn");
+  const closeModalBtn = document.getElementsByClassName("close-modal")[0];
 
-const birthReportModal = document.getElementById("birth-report-modal");
-const birthReportModalContent = document.getElementById("modal-1");
-const birthReportBtn = document.getElementById("give-birth-btn");
-const closeModalBtn = document.getElementsByClassName("close-modal")[0];
+  const cancelBtn = document.getElementsByClassName("cancel-btn")[0];
 
-const cancelBtn = document.getElementsByClassName("cancel-btn")[0];
+  const sidebar = document.getElementById("side-bar");
+  const dashboard = document.getElementById("dashboard");
+  const myPage = document.getElementById("my-page");
+  const myTask = document.getElementById("my-task");
+  const generatePlan = document.getElementById("generate-plan");
 
-const sidebar = document.getElementById("side-bar");
-const dashboard = document.getElementById("dashboard");
-const myPage = document.getElementById("my-page");
-const myTask = document.getElementById("my-task");
-const generatePlan = document.getElementById("generate-plan");
+  const miniNavbarAnimation = document.getElementById("mini-navbar-animation");
 
-// Open Modal
-birthReportBtn.addEventListener('click', () => {
-  loadComponents("birth-report-modal", `${path}/components/modal1.html`);
-})
+  const navItems = [
+    basicInfo,
+    wageInfo,
+    familyInfo,
+    parentalLeaveInfo,
+    accountSettings,
+  ];
 
-// Open Mini-tabs
-loadComponents("dynamic-content", `${path}/myPage/basicInfo.html`);
+  // Open Mini-tabs
+  loadComponents("dynamic-content", `${path}/myPage/basicInfo.html`);
 
-wageInfo.addEventListener('click', () => {
-  loadComponents("dynamic-content", `${path}/myPage/wageInfo.html`);
-})
-
-familyInfo.addEventListener('click', () => {
-  loadComponents("dynamic-content", `${path}/myPage/familyInfo.html`);
-})
-
-parentalLeaveInfo.addEventListener('click', () => {
-  loadComponents("dynamic-content", `${path}/myPage/parentalLeaveInfo.html`);
-})
-
-accountSettings.addEventListener('click', () => {
-  loadComponents("dynamic-content", `${path}/myPage/accountSettings.html`);
-})
-
-// Navigate to My Task
-myTask.addEventListener("click", (e) => {
-  window.location.href =
-    `${path}/myTask/index.html`;
-});
-
-
-
-
-
-const miniNavbarAnimation = document.getElementById("mini-navbar-animation");
-
-const navItems = [
-  basicInfo,
-  wageInfo,
-  familyInfo,
-  parentalLeaveInfo,
-  accountSettings,
-];
-
-// Modal
-const displayModal = () => {
-  birthReportModal.classList.add("show");
-  birthReportModalContent.style.animation = "zoomIn 0.5s forwards";
-  birthReportModal.style.display = "block";
-};
-
-const closeModal = () => {
-  birthReportModalContent.style.animation = "zoomOut 0.5s forwards";
-  setTimeout(() => {
-    birthReportModal.style.display = "none";
-  }, 500); // Match the duration of the animation
-};
-
-birthReportBtn.addEventListener("click", displayModal);
-[closeModalBtn, cancelBtn].forEach((btn) =>
-  btn.addEventListener("click", closeModal)
-);
-window.addEventListener("click", (e) => {
-  if (e.target === birthReportModal) {
-    closeModal();
-  }
-});
-
-// 1st tab
-
-// const createBasicInfoContent = () => {
-//   const basicInfoItems = [
-//     "Employee Number",
-//     "Employee Name",
-//     "Department",
-//     "Role",
-//     "Type of Employment",
-//     "Gender",
-//     "Date of Birth",
-//   ];
-
-//   const basicInfoContent = document.createElement("div");
-//   basicInfoContent.setAttribute("id", "basic-info-content");
-//   basicInfoContent.innerHTML = [...Array(7).keys()].reduce(
-//     (prev, current, idx) => {
-//       return (
-//         prev + `<label>${basicInfoItems[idx]}</label><br><fieldset></fieldset>`
-//       );
-//     },
-//     ""
-//   );
-//   dynamicContentSection.appendChild(basicInfoContent);
-
-  navItems.forEach((item) => {
-    item.addEventListener("click", () => {
-      navItems.forEach((nav) => nav.classList.remove("active"));
-      item.classList.add("active");
+  if (basicInfo) {
+    basicInfo.addEventListener("click", () => {
+      dynamicContentSection.textContent = "";
+      loadComponents("dynamic-content", `${path}/myPage/basicInfo.html`);
     });
+
+    miniNavbarAnimation.style.width = "10rem";
+    miniNavbarAnimation.style.left = "0";
+    miniNavbarAnimation.style.transition = "0.2s";
+  }
+
+  if (wageInfo) {
+    wageInfo.addEventListener("click", () => {
+      dynamicContentSection.textContent = "";
+      loadComponents("dynamic-content", `${path}/myPage/wageInfo.html`);
+
+      miniNavbarAnimation.style.width = "8rem";
+      miniNavbarAnimation.style.left = "12rem";
+      miniNavbarAnimation.style.transition = "0.2s";
+    });
+  }
+
+  if (familyInfo) {
+    familyInfo.addEventListener("click", () => {
+      dynamicContentSection.textContent = "";
+      loadComponents("dynamic-content", `${path}/myPage/familyInfo.html`);
+
+      miniNavbarAnimation.style.width = "17rem";
+      miniNavbarAnimation.style.left = "22.5rem";
+      miniNavbarAnimation.style.transition = "0.2s";
+    });
+  }
+
+  if (parentalLeaveInfo) {
+    parentalLeaveInfo.addEventListener("click", () => {
+      dynamicContentSection.textContent = "";
+      loadComponents(
+        "dynamic-content",
+        `${path}/myPage/parentalLeaveInfo.html`
+      );
+
+      miniNavbarAnimation.style.width = "13.5rem";
+      miniNavbarAnimation.style.left = "42rem";
+      miniNavbarAnimation.style.transition = "0.2s";
+    });
+  }
+
+  if (accountSettings) {
+    accountSettings.addEventListener("click", () => {
+      dynamicContentSection.textContent = "";
+      loadComponents("dynamic-content", `${path}/myPage/accountSettings.html`);
+
+      miniNavbarAnimation.style.width = "11.5rem";
+      miniNavbarAnimation.style.left = "59rem";
+      miniNavbarAnimation.style.transition = "0.2s";
+    });
+  }
+
+  // Navigate to My Task
+  if (myTask) {
+    myTask.addEventListener("click", (e) => {
+      window.location.href = `${path}/myTask/index.html`;
+    });
+  }
+
+  // Modal
+  const displayModal = () => {
+    birthReportModal.classList.add("show");
+    birthReportModalContent.style.animation = "zoomIn 0.5s forwards";
+    birthReportModal.style.display = "block";
+  };
+
+  const closeModal = () => {
+    birthReportModalContent.style.animation = "zoomOut 0.5s forwards";
+    setTimeout(() => {
+      birthReportModal.style.display = "none";
+    }, 500); // Match the duration of the animation
+  };
+
+  // Open Modal
+  if (birthReportBtn) {
+    birthReportBtn.addEventListener("click", () => {
+      loadComponents("birth-report-modal", `${path}/components/modal1.html`);
+      displayModal();
+    });
+  }
+
+  // Close Modal
+
+  if (closeModalBtn && cancelBtn) {
+    [closeModalBtn, cancelBtn].forEach((btn) =>
+      btn.addEventListener("click", closeModal)
+    );
+    window.addEventListener("click", (e) => {
+      if (e.target === birthReportModal) {
+        closeModal();
+      }
+    });
+  }
+
+  // Navigate mini-tabs
+  navItems.forEach((item) => {
+    if (item) {
+      item.addEventListener("click", () => {
+        navItems.forEach((nav) => nav.classList.remove("active"));
+        item.classList.add("active");
+      });
+    }
   });
 
-createBasicInfoContent();
+  // 1st tab
 
-basicInfo.addEventListener("click", () => {
-  dynamicContentSection.textContent = "";
-  createBasicInfoContent();
+  // const createBasicInfoContent = () => {
+  //   const basicInfoItems = [
+  //     "Employee Number",
+  //     "Employee Name",
+  //     "Department",
+  //     "Role",
+  //     "Type of Employment",
+  //     "Gender",
+  //     "Date of Birth",
+  //   ];
 
-  miniNavbarAnimation.style.width = "10rem";
-  miniNavbarAnimation.style.left = "0";
-  miniNavbarAnimation.style.transition = "0.2s";
-});
+  //   const basicInfoContent = document.createElement("div");
+  //   basicInfoContent.setAttribute("id", "basic-info-content");
+  //   basicInfoContent.innerHTML = [...Array(7).keys()].reduce(
+  //     (prev, current, idx) => {
+  //       return (
+  //         prev + `<label>${basicInfoItems[idx]}</label><br><fieldset></fieldset>`
+  //       );
+  //     },
+  //     ""
+  //   );
+  //   dynamicContentSection.appendChild(basicInfoContent);
 
-// 2nd tab
-wageInfo.addEventListener("click", () => {
-  dynamicContentSection.textContent = "";
-  const wageInfoItems = ["Wage (Monthly)"];
-  const wageInfoContent = document.createElement("div");
-  wageInfoContent.setAttribute("id", "wage-info-content");
-  wageInfoContent.innerHTML = `<label>${wageInfoItems}</label><br><fieldset></fieldset>`;
-  dynamicContentSection.appendChild(wageInfoContent);
+  // // 2nd tab
+  // wageInfo.addEventListener("click", () => {
+  //   dynamicContentSection.textContent = "";
+  //   const wageInfoItems = ["Wage (Monthly)"];
+  //   const wageInfoContent = document.createElement("div");
+  //   wageInfoContent.setAttribute("id", "wage-info-content");
+  //   wageInfoContent.innerHTML = `<label>${wageInfoItems}</label><br><fieldset></fieldset>`;
+  //   dynamicContentSection.appendChild(wageInfoContent);
 
-  miniNavbarAnimation.style.width = "8rem";
-  miniNavbarAnimation.style.left = "12rem";
-  miniNavbarAnimation.style.transition = "0.2s";
-});
+  //   miniNavbarAnimation.style.width = "8rem";
+  //   miniNavbarAnimation.style.left = "12rem";
+  //   miniNavbarAnimation.style.transition = "0.2s";
+  // });
 
-// 3rd tab
-familyInfo.addEventListener("click", () => {
-  dynamicContentSection.textContent = "";
-  const familyInfoItems = [
-    "Number of Dependents",
-    "Numnber of Children",
-    "Family Address",
-    "Name of Spouse",
-    "Date of Birth of Spouse",
-    "Employment Status of Spouse",
-    "Salary of Spouse",
-  ];
-  const familyInfoContent = document.createElement("div");
-  familyInfoContent.setAttribute("id", "family-info-content");
-  familyInfoContent.innerHTML = [...Array(7).keys()].reduce(
-    (prev, current, idx) => {
-      return (
-        prev + `<label>${familyInfoItems[idx]}</label><br><fieldset></fieldset>`
-      );
-    },
-    ""
-  );
-  dynamicContentSection.appendChild(familyInfoContent);
+  // // 3rd tab
+  // familyInfo.addEventListener("click", () => {
+  //   dynamicContentSection.textContent = "";
+  //   const familyInfoItems = [
+  //     "Number of Dependents",
+  //     "Numnber of Children",
+  //     "Family Address",
+  //     "Name of Spouse",
+  //     "Date of Birth of Spouse",
+  //     "Employment Status of Spouse",
+  //     "Salary of Spouse",
+  //   ];
+  //   const familyInfoContent = document.createElement("div");
+  //   familyInfoContent.setAttribute("id", "family-info-content");
+  //   familyInfoContent.innerHTML = [...Array(7).keys()].reduce(
+  //     (prev, current, idx) => {
+  //       return (
+  //         prev + `<label>${familyInfoItems[idx]}</label><br><fieldset></fieldset>`
+  //       );
+  //     },
+  //     ""
+  //   );
+  //   dynamicContentSection.appendChild(familyInfoContent);
 
-  miniNavbarAnimation.style.width = "17rem";
-  miniNavbarAnimation.style.left = "22.5rem";
-  miniNavbarAnimation.style.transition = "0.2s";
-});
+  //   miniNavbarAnimation.style.width = "17rem";
+  //   miniNavbarAnimation.style.left = "22.5rem";
+  //   miniNavbarAnimation.style.transition = "0.2s";
+  // });
 
-// 4th tab
-parentalLeaveInfo.addEventListener("click", () => {
-  dynamicContentSection.textContent = "";
-  const parentalLeaveItems = [
-    "Receiving Leave Desire",
-    "Address for Leaving Period",
-    "Date of Expiry of Address for Leaving Period",
-    "Contact of Address for Leaving Period",
-  ];
-  const parentalLeaveContent = document.createElement("div");
-  parentalLeaveContent.setAttribute("id", "parental-leave-content");
-  parentalLeaveContent.innerHTML = [...Array(4).keys()].reduce(
-    (prev, current, idx) => {
-      return (
-        prev +
-        `<label>${parentalLeaveItems[idx]}</label><br><fieldset></fieldset>`
-      );
-    },
-    ""
-  );
-  dynamicContentSection.appendChild(parentalLeaveContent);
+  // // 4th tab
+  // parentalLeaveInfo.addEventListener("click", () => {
+  //   dynamicContentSection.textContent = "";
+  //   const parentalLeaveItems = [
+  //     "Receiving Leave Desire",
+  //     "Address for Leaving Period",
+  //     "Date of Expiry of Address for Leaving Period",
+  //     "Contact of Address for Leaving Period",
+  //   ];
+  //   const parentalLeaveContent = document.createElement("div");
+  //   parentalLeaveContent.setAttribute("id", "parental-leave-content");
+  //   parentalLeaveContent.innerHTML = [...Array(4).keys()].reduce(
+  //     (prev, current, idx) => {
+  //       return (
+  //         prev +
+  //         `<label>${parentalLeaveItems[idx]}</label><br><fieldset></fieldset>`
+  //       );
+  //     },
+  //     ""
+  //   );
+  //   dynamicContentSection.appendChild(parentalLeaveContent);
 
-  miniNavbarAnimation.style.width = "13.5rem";
-  miniNavbarAnimation.style.left = "42rem";
-  miniNavbarAnimation.style.transition = "0.2s";
-});
+  //   miniNavbarAnimation.style.width = "13.5rem";
+  //   miniNavbarAnimation.style.left = "42rem";
+  //   miniNavbarAnimation.style.transition = "0.2s";
+  // });
 
-// 5th tab
-accountSettings.addEventListener("click", () => {
-  dynamicContentSection.textContent = "";
-  const accountSettingsItems = ["Mail Address", "Password"];
-  const accountSettingsContent = document.createElement("div");
-  accountSettingsContent.setAttribute("id", "account-settings-content");
-  accountSettingsContent.innerHTML = `<label>${accountSettingsItems[0]}</label>
-  <br>
-  <span>This account is being registered by this Email Address:</span>
-  <label>${accountSettingsItems[1]}</label><br>
-  <span>Change your Password</span>`;
+  // // 5th tab
+  // accountSettings.addEventListener("click", () => {
+  //   dynamicContentSection.textContent = "";
+  //   const accountSettingsItems = ["Mail Address", "Password"];
+  //   const accountSettingsContent = document.createElement("div");
+  //   accountSettingsContent.setAttribute("id", "account-settings-content");
+  //   accountSettingsContent.innerHTML = `<label>${accountSettingsItems[0]}</label>
+  //   <br>
+  //   <span>This account is being registered by this Email Address:</span>
+  //   <label>${accountSettingsItems[1]}</label><br>
+  //   <span>Change your Password</span>`;
 
-  dynamicContentSection.appendChild(accountSettingsContent);
+  //   dynamicContentSection.appendChild(accountSettingsContent);
 
-  miniNavbarAnimation.style.width = "11.5rem";
-  miniNavbarAnimation.style.left = "59rem";
-  miniNavbarAnimation.style.transition = "0.2s";
+  //   miniNavbarAnimation.style.width = "11.5rem";
+  //   miniNavbarAnimation.style.left = "59rem";
+  //   miniNavbarAnimation.style.transition = "0.2s";
+  // });
 });
